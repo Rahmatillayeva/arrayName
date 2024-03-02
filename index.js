53a1d8b633069a80fb38e0ae342f4bfcc27839ac
Rahmatillayeva <shaxrizodamrahmatillayeva@gmail.com>
2024-03-02 22:15:41 +0500

    harflar massividan ism hosil qilish

---
+++ index.js
@@ -0,0 +1,35 @@
+// const ism = [];
+
+let harflar = {
+ a: ["a", "A"],
+ b: ["b", "B"],
+ C: ["c", "C"],
+ d: ["d", "D"],
+ e: ["e", "E"],
+ f: ["f", "F"],
+ g: ["g", "G"],
+ h: ["h", "H"],
+ i: ["i", "I"],
+ j: ["j", "J"],
+ k: ["k", "K"],
+ l: ["l", "L"],
+ m: ["m", "M"],
+ n: ["n", "N"],
+ o: ["o", "O"],
+ p: ["p", "P"],
+ q: ["q", "Q"],
+ r: ["r", "R"],
+ s: ["s", "S"],
+ t: ["t", "T"],
+ u: ["u", "U"],
+ v: ["v", "V"],
+ x: ["x", "X"],
+ y: ["y", "Y"],
+ z: ["z", "Z"],
+}
+// ism.push(harflar.s, harflar.h,harflar.a, harflar.x, harflar.r, harflar.i, harflar.z, harflar.o, harflar.d, harflar.a)
+
+console.log(harflar.r[1], harflar.a[0],harflar.h[0], harflar.m[0], harflar.a[0], harflar.t[0], harflar.i[0], harflar.l[0], harflar.l[0], harflar.a[0], harflar.y[0], harflar.e[0], harflar.v[0], harflar.a[0]);
+
+
+console.log(harflar.s[1], harflar.h[0],harflar.a[0], harflar.h[0], harflar.r[0], harflar.i[0], harflar.z[0], harflar.o[0], harflar.d[0], harflar.a[0]);
